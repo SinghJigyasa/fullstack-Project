ca52cbe79a4ec59a363b4ef7b58b4bb04e4eb067
Jigyasa Singh <jig86026@gmail.com>
2024-04-23 18:43:25 +0530

    ui changes

--- src/layout/mainLayout.js
+++ src/layout/mainLayout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const MainLayoutComp = () => {
   return (
     <div>
-      <header className="bg-dark text-white ">
+        <header className="bg-dark text-white d-flex align-items-center justify-content-center fs-1" style={{ height: '25vh' }}>
           Wlecome to Jigyasa Fullstack Project
       </header>
       <Outlet/>
